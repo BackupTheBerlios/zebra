75f80d1dda42813f6d1515ebbb0ce48cc3de063e
bgidley <bgidley>
2005-07-24 10:15:11 +0000

    Initial Import with Empty Interface

---
+++ zebra/src/java/zebra-hivemind/src/java/com/anite/zebra/hivemind/api/Zebra.java
@@ -0,0 +1,15 @@
+package com.anite.zebra.hivemind.api;
+
+/**
+ * The Core Zebra Facade Interface for Hivemind
+ * 
+ * All objects and common functions required should be available from here.
+ * All implementation is handled via Hivemind - therefore each application can implement it however it wants. 
+ * 
+ * This replaces classes like ZebraHelper and the Avalon Helpers seen in the other implementations of Zebra.
+ * 
+ * @author Ben Gidley 
+ */
+public class Zebra {
+
+}
