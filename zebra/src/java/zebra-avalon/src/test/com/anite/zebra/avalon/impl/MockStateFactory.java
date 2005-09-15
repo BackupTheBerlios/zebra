952c771cf4fc81d237975093354f3d6c4df65cb8
mpn_uk <mpn_uk>
2005-09-15 17:02:07 +0000

    Updated boilerplates.

--- zebra/src/java/zebra-avalon/src/test/com/anite/zebra/avalon/impl/MockStateFactory.java
+++ zebra/src/java/zebra-avalon/src/test/com/anite/zebra/avalon/impl/MockStateFactory.java
@@ -1,9 +1,22 @@
 /*
- * Created on Aug 5, 2004
+ * Copyright 2004/2005 Anite - Enforcement & Security
+ *    http://www.anite.com/publicsector
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.anite.zebra.avalon.impl;
 
+import com.anite.zebra.core.api.IEngine;
 import com.anite.zebra.core.definitions.api.IProcessDefinition;
 import com.anite.zebra.core.definitions.api.ITaskDefinition;
 import com.anite.zebra.core.exceptions.LockException;
@@ -78,6 +91,22 @@ public class MockStateFactory implements IStateFactory {
 	}
 
 	public void releaseLock(IProcessInstance arg0) throws LockException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.anite.zebra.core.factory.api.IStateFactory#acquireLock(com.anite.zebra.core.state.api.IProcessInstance, com.anite.zebra.core.api.IEngine)
+	 */
+	public void acquireLock(IProcessInstance arg0, IEngine arg1) throws LockException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.anite.zebra.core.factory.api.IStateFactory#releaseLock(com.anite.zebra.core.state.api.IProcessInstance, com.anite.zebra.core.api.IEngine)
+	 */
+	public void releaseLock(IProcessInstance arg0, IEngine arg1) throws LockException {
 		// TODO Auto-generated method stub
 		
 	}
