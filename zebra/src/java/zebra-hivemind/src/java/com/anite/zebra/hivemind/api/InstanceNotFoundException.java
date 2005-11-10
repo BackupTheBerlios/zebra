76f70c8766bc24b93eb359059d6e580af958a779
bgidley <bgidley>
2005-11-10 17:30:24 +0000

    *** empty log message ***

---
+++ zebra/src/java/zebra-hivemind/src/java/com/anite/zebra/hivemind/api/InstanceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.anite.zebra.hivemind.api;
+
+import org.apache.commons.lang.exception.NestableException;
+
+/**
+ * Exception for when the helper cannot locate the requested task or process instance
+ * @author ben.gidley
+ *
+ */
+public class InstanceNotFoundException extends NestableException {
+
+	private static final long serialVersionUID = 1L;
+
+}
--- zebra/src/java/zebra-hivemind/src/java/com/anite/zebra/hivemind/api/Zebra.java
+++ zebra/src/java/zebra-hivemind/src/java/com/anite/zebra/hivemind/api/Zebra.java
@@ -1,10 +1,17 @@
 package com.anite.zebra.hivemind.api;
 
+import java.util.List;
 import java.util.Map;
 
 import com.anite.zebra.core.exceptions.DefinitionNotFoundException;
-import com.anite.zebra.ext.definitions.impl.ProcessDefinition;
-import com.anite.zebra.ext.definitions.impl.TaskDefinition;
+import com.anite.zebra.core.exceptions.RunTaskException;
+import com.anite.zebra.core.exceptions.StartProcessException;
+import com.anite.zebra.core.exceptions.TaskConstructException;
+import com.anite.zebra.core.exceptions.TransitionException;
+import com.anite.zebra.core.factory.exceptions.ClassInstantiationException;
+import com.anite.zebra.core.factory.exceptions.StateFailureException;
+import com.anite.zebra.core.state.api.IProcessInstance;
+import com.anite.zebra.core.state.api.ITaskInstance;
 
 /**
  * The Core Zebra Facade Interface for Hivemind
@@ -18,10 +25,11 @@ import com.anite.zebra.ext.definitions.impl.TaskDefinition;
  * 
  * @author Ben Gidley
  */
-public interface Zebra {
+public interface Zebra<PD, TD, PI, TI> {
 
 	/**
 	 * returns all process definitions as a map
+	 * 
 	 * @return
 	 */
 	public Map getAllProcessDefinitions();
@@ -34,9 +42,47 @@ public interface Zebra {
 	 * @throws DefinitionNotFoundException
 	 *             definition not found exception
 	 */
-	public ProcessDefinition getProcessDefinition(String processName)
-			throws DefinitionNotFoundException;
+	public PD getProcessDefinitions() throws DefinitionNotFoundException;
 
-	public TaskDefinition getTaskDefinition(String taskName)
-			throws DefinitionNotFoundException;
+	/**
+	 * Create a process in a paused state
+	 * @param processName
+	 * @return
+	 */
+	public PI createProcessPaused(String processName);
+
+	/**
+	 * Get the passed users task list include all owned and delegated tasks
+	 * @param user
+	 * @return
+	 */
+	public List<TI> getTaskList(User user);
+
+	/**
+	 * Get the passed users tasks list but only directly owned tasks
+	 * @param user
+	 * @return
+	 */
+	public List<TI> getOnlyOwnedTaskList(User user);
+
+	/**
+	 * Get the passed users tasks list but only delegated tasks
+	 * @param user
+	 * @return
+	 */
+	public List<TI> getOnlyDelegatedTaskList(User user);
+
+	/**
+	 * Start the process
+	 * @param processInstance
+	 * @throws StartProcessException
+	 */
+	public void startProcess(PI processInstance) throws StartProcessException;
+
+	/**
+	 * Transition the passed task
+	 * @param taskInstance
+	 * @throws TransitionException
+	 */
+	public void transitionTask(TI taskInstance) throws TransitionException;
 }
