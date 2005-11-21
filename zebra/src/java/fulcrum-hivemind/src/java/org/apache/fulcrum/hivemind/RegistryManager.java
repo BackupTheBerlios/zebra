610150426da5762593547ec66bf706f5eae2dcf6
bgidley <bgidley>
2005-11-21 14:05:19 +0000

    *** empty log message ***

--- zebra/src/java/fulcrum-hivemind/src/java/org/apache/fulcrum/hivemind/RegistryManager.java
+++ zebra/src/java/fulcrum-hivemind/src/java/org/apache/fulcrum/hivemind/RegistryManager.java
@@ -1,17 +1,25 @@
 package org.apache.fulcrum.hivemind;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import org.apache.hivemind.Registry;
+import org.apache.hivemind.Resource;
+import org.apache.hivemind.impl.DefaultClassResolver;
 import org.apache.hivemind.impl.RegistryBuilder;
+import org.apache.hivemind.impl.XmlModuleDescriptorProvider;
 
 public class RegistryManager {
 
-    private Registry registry;
+    private Registry registry = null;
+
+    private List<Resource> resources = new ArrayList<Resource>();
 
     private static RegistryManager _instance;
 
     private RegistryManager() {
-        this.registry = constructRegistry();
+        
     }
 
     /**
@@ -19,8 +27,17 @@ public class RegistryManager {
      * @return
      */
     protected Registry constructRegistry() {
-        
-        return RegistryBuilder.constructDefaultRegistry();
+
+        RegistryBuilder builder = new RegistryBuilder();
+
+        builder.addDefaultModuleDescriptorProvider();
+
+        if (resources.size() > 0) {
+            builder.addModuleDescriptorProvider(new XmlModuleDescriptorProvider(new DefaultClassResolver(), resources));
+        }
+
+        return builder.constructRegistry(Locale.getDefault());
+
     }
 
     public static RegistryManager getInstance() {
@@ -31,9 +48,23 @@ public class RegistryManager {
     }
 
     public Registry getRegistry() {
+        if (this.registry==null){
+            this.registry = constructRegistry();
+        }        
         return this.registry;
     }
-    
-    
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    /**
+     * Call the prior to first call to getInstance to override default module list
+     * This is typically used in Unit Testing to swap an implementation
+     * @param resources
+     */
+    public void setResources(List<Resource> resources) {
+        this.resources = resources;
+    }
 
 }
