04b58ae2ebaf990905cf5f57dcd6a7c05890674d
bgidley <bgidley>
2005-12-01 13:58:08 +0000

    *** empty log message ***

--- zebra/src/java/zebra-hivemind/src/test/java/com/anite/zebra/hivemind/impl/ZebraSecurityManagerTest.java
+++ zebra/src/java/zebra-hivemind/src/test/java/com/anite/zebra/hivemind/impl/ZebraSecurityManagerTest.java
@@ -16,12 +16,14 @@
  */
 package com.anite.zebra.hivemind.impl;
 
+import junit.framework.TestCase;
+
 import org.apache.fulcrum.hivemind.RegistryManager;
+import org.apache.fulcrum.security.entity.Permission;
+import org.apache.fulcrum.security.util.PermissionSet;
 import org.apache.hivemind.Resource;
 import org.apache.hivemind.impl.DefaultClassResolver;
 import org.apache.hivemind.util.ClasspathResource;
-
-import junit.framework.TestCase;
 
 public class ZebraSecurityManagerTest extends TestCase {
 
@@ -43,5 +45,37 @@ public class ZebraSecurityManagerTest extends TestCase {
         assertNotNull(security);
         assertNotNull(security.getPermissionManager());
     }
+    
+    /** 
+     * Test getPermission test related methods
+     * We only need to call the version using a string as the other one is call by that
+     * 
+     */
+    public void testGetPermissionSet(){
+        ZebraSecurity security = (ZebraSecurity) RegistryManager.getInstance().getRegistry().getService(
+                "zebra.zebraSecurity", ZebraSecurity.class);
+        
+        PermissionSet results = security.getPermissionSet("bob;jack;harry");
+        assertEquals(results.size(), 3);
+        
+        Permission[] permissions = results.getPermissionsArray();
+        for (Permission permission : permissions) {
+            assertNotNull(permission);
+            assertNotNull(permission.getName());
+        }
+    }
+    
+    public void testGetTaskList(){
+       // TODO write me
+        
+        // Create a user object with a permission
+        
+        // Start a flow with permissions on it that the user has
+        
+        // Check if the task is on the list
+        
+        // start a flow without this permission and then check it is not on the list
+    }
+    
 
 }
